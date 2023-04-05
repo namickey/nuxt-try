56fbca09700ce401d9d32f6c69d6a838c56584b8
namickey <teethdai@gmail.com>
2023-04-05 21:52:28 +0900

    add setup

---
+++ setup-ec2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo 'whoami'
+whoami
+
+echo 'cd /home/ec2-user'
+cd /home/ec2-user
+
+echo 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash'
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+echo 'ls -la'
+ls -la
+
+echo '. /home/ec2-user/.nvm/nvm.sh'
+. /home/ec2-user/.nvm/nvm.sh
+
+echo 'nvm install --lts'
+nvm install --lts
+
+echo 'cd /home/ec2-user/nuxt-try'
+cd /home/ec2-user/nuxt-try
+
+echo 'npm install -Y'
+npm install -Y
+
+echo 'npm run dev &'
+npm run dev &
