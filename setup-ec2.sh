90a2fdd0367afc969e50556445beff8ac3d1e25b
namickey <teethdai@gmail.com>
2023-04-09 06:12:26 +0900

    fix

--- setup-ec2.sh
+++ setup-ec2.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 
-echo 'whoami'
-whoami
-
-echo 'cd /home/ec2-user'
 cd /home/ec2-user
 
-echo 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash'
+# nvm install
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-
-echo 'ls -la'
-ls -la
-
-echo '. /home/ec2-user/.nvm/nvm.sh'
 . /home/ec2-user/.nvm/nvm.sh
 
-echo 'nvm install --lts'
+# npm install
 nvm install --lts
 
-echo 'cd /home/ec2-user/nuxt-try'
+# パッケージインストール
 cd /home/ec2-user/nuxt-try
-
-echo 'yes npm install'
 yes | npm install
 
-echo 'npm run dev &'
-npm run dev &
+# ビルド & 起動
+npm run build
+node .output/server/index.mjs &
